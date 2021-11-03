deba30c250ef4b3173018da3acc1fd66825a1b44
Ailln <kinggreenhall@gmail.com>
2021-11-04 01:42:04 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,48 @@
+import sys
+from setuptools import setup
+from setuptools import find_packages
+
+# change this.
+NAME = "your package name"
+VERSION = "0.0.1"
+AUTHOR = "your user name"
+EMAIL = "your email"
+URL = "your project url"
+LICENSE = "your license"
+DESCRIPTION = "your project description"
+
+if sys.version_info[:2] < (3, 6):
+    raise RuntimeError("Python version >= 3.6 required.")
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=EMAIL,
+        url=URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=open("./requirements.txt", "r").read().splitlines(),
+        long_description=open("./README.md", "r").read(),
+        long_description_content_type='text/markdown',
+        # change package_name to your package name.
+        entry_points={
+            "console_scripts": [
+                "package_name=package_name.shell:run"
+            ]
+        },
+        package_data={
+            # change package_name to your package name.
+            "package_name": ["src/*.txt"]
+        },
+        zip_safe=True,
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            f"License :: OSI Approved :: {LICENSE}",
+            "Operating System :: OS Independent",
+        ],
+        python_requires=">=3.6"
+    )
